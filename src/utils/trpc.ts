78165ded32e65c9cee3b4a20096049bda8a9f7d6
James Moran <github@alias.jamesmoran.uk>
2023-02-03 14:19:22 +0000

    Remove connection from trpc

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -54,7 +54,7 @@ export const trpc = createTRPCNext<AppRouter>({
               // If you're using Node 18, omit the "connection" header
               const {
                 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                connection: _connection,
+                // connection: _connection,
                 ...headers
               } = ctx.req.headers;
               return {
